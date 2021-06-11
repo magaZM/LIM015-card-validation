905059d3fbd605ee571ad67c313636f8d741eede
magaZM <aurazambrano227@gmail.com>
2021-06-11 09:22:01 -0500

    first version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,146 @@
+// Importamos el objeto `validator`, que contiene las funciones `isValid` y `maskify`
 import validator from './validator.js';
+import { validity } from './validator.js';
 
-console.log(validator);
+// Elementos HTML
+const btnVerify = document.querySelector('.btn');
+const inputNumber = document.querySelector('#number');
+const containerInput = document.querySelector('.container-input');
+const containerResults = document.querySelector('.container-results');
+
+// Variables Globales
+let icon;
+let creditCardNumber;
+
+// Eventos 
+document.addEventListener('DOMContentLoaded', startApp);
+inputNumber.addEventListener('keyup', disabledBtn);
+btnVerify.addEventListener('click', validateForm);
+btnVerify.addEventListener("keydown", (e) => {
+     if (e.keyCode === 13 || e.key === 'Enter') {  //checks whether the pressed key is "Enter"
+        validateForm
+     }
+})
+
+// Función para verificar cuando la página carga y desabilitar el botón
+function startApp() {
+    btnVerify.disabled = true;
+    btnVerify.classList.add('disabled');
+}
+
+// Función para validar el número de tarjeta e imprimir los detalles de la operación
+function validateForm() {
+    creditCardNumber = inputNumber.value;
+    const regEXp = /\D|\s$/;
+
+    if (regEXp.test(creditCardNumber)){
+        inputNumber.value = '';
+        showAlert('Ingresa solo números por favor');
+    }
+    else {
+        validator.isValid(creditCardNumber);
+        validator.maskify(creditCardNumber);
+
+        if (validator.isValid(creditCardNumber)){
+            showAlert('Tarjeta Válida');
+            showDetails();
+        } else {
+            showAlert('Tarjeta Inválida');
+            showDetails();
+        }
+    }
+}
+
+// Función para habilitar el boton de verificar cuando escribe en el input
+function disabledBtn() {
+
+    if (inputNumber.value.length >= 13) {
+        btnVerify.classList.remove('disabled');
+        btnVerify.disabled = false;
+    } else {
+        startApp();
+    }
+}
+
+// Función para eliminar mensajes de alerta
+function deleteAlert() {
+    // Verifica si existe una alerta y la remueve 
+    const existsDiv = document.querySelector('.alert');
+    if (existsDiv !== null) {
+        const parentNode = existsDiv.parentNode;
+        parentNode.removeChild( existsDiv );
+    }
+}
+
+// Función para mostrar mensajes de alerta
+function showAlert(message) {
+
+    deleteAlert()
+    const divAlert = document.createElement('div');
+    divAlert.classList.add('text-center');
+    
+
+    if (validity === 'Válida'){
+        icon = '<svg class="w-6 h-6" fill="none" stroke="currentColor" width="16px" color="green"  viewBox="5 5 15 15" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>'
+        divAlert.classList.add('success', 'alert')
+    } else {
+        icon = '<svg class="w-6 h-6" fill="none" stroke="currentColor" width="12px" color="red" viewBox="5 5 15 15" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>'
+        divAlert.classList.add('error', 'alert')
+    }
+    divAlert.innerHTML = `${icon} ${message}`;
+    containerInput.insertBefore(divAlert, containerInput.children[0]);
+
+}
+
+// Función para mostrar mensaje con detalle de la operación
+function showDetails() {
+
+    // Muestra el container de resultados y agrega clase
+    containerResults.style.display = 'block';
+    containerResults.classList.add('modal-container');
+
+    //Crea el contenido del container Resultados en un div
+    const divDetails = document.createElement('div');
+    divDetails.innerHTML = `
+    <table>
+        <thead>
+            <tr> 
+                <th colspan="2" class="text-center"> Detalles de la tarjeta: </th> 
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr> 
+                <th> Número de tarjeta:  </th>
+                <td> ${validator.maskify(creditCardNumber)} </td>
+            </tr>
+            <tr> 
+                <th> Marca de tarjeta:  </th>
+                <td> ${validator.franchiseCard(creditCardNumber)} </td>
+            </tr>
+            <tr> 
+                <th> Validación:  </th>
+                <td> ${icon} ${validity} </td>
+            </tr>
+        </tbody>
+    
+    </table>
+    <button type="submit" class="btn" id="goBack" >Reiniciar</button>
+    `
+    divDetails.classList.add('show', 'modal')
+    containerResults.appendChild(divDetails);
+
+    // Agrega evento al botón de goBack para volver a la ventana principal
+    const btnGoBack = document.querySelector('#goBack');
+    btnGoBack.addEventListener('click', () => {
+        // Remueve la clase del modal
+        containerResults.classList.remove('modal-active', 'show');
+        // Limpia el input y el div con mensaje anterior
+        inputNumber.value = '';
+        containerResults.innerHTML = '';
+        // oculta el div de resultados 
+        containerResults.style.display = 'none';
+        //Elimina mensajes de alerta
+        deleteAlert()
+    })
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,74 @@
+export let validity;
+
 const validator = {
-  // ...
+
+  isValid: (creditCardNumber) => {
+    
+    const convertToArray = Array.from(creditCardNumber).reverse();
+
+    let sum = 0;
+
+    for(let index = 1; index < convertToArray.length; index += 2){
+    
+      const multiplyPairs = convertToArray[index] * 2;
+
+      if (multiplyPairs >= 10) {
+        const convertToString = multiplyPairs.toString();
+        const firstPosition = convertToString[0];
+        const lastPosition = convertToString[1];
+        convertToArray[index] = Number(firstPosition) + Number(lastPosition);
+      } else {
+        convertToArray[index] = multiplyPairs;
+      }
+    }
+
+    convertToArray.forEach(element => sum = sum + Number(element))
+
+    if(sum % 10 === 0){
+      validity = 'Válida';
+      return true;
+    }else {
+      validity = 'Inválida';
+      return false;
+    }
+  },
+
+  maskify: (creditCardNumber) =>  {
+   
+    const convertCreditCardNumberToArray = Array.from(creditCardNumber);
+    const regExpArr = /\d$/g;
+    for(let index = 0; index < convertCreditCardNumberToArray.length - 4; index++){
+      
+      convertCreditCardNumberToArray[index] = convertCreditCardNumberToArray[index].replace(regExpArr, '#');
+
+    }
+  
+    let newCreditCardNumber = convertCreditCardNumberToArray.toString();
+
+    while(newCreditCardNumber.indexOf(',') != -1 ) {
+      newCreditCardNumber = newCreditCardNumber.toString().replace(',', '');
+    }
+    return newCreditCardNumber;
+  }, 
+
+  franchiseCard: (creditCardNumber) => {
+
+    const regExpFranchise = {
+      Maestro: /^(5018|5020|5038|5612|5893|6304|6759|6761|6762|6763|0604|6390)\d+$/,
+      Visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+      Mastercard: /^5[1-5][0-9]{14}$/,
+      'American Express': /^3[47][0-9]{13}$/,
+      Diners: /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/,
+      Discover: /^6(?:011|5[0-9]{2})[0-9]{12}$/,
+      Jcb: /^(?:2131|1800|35\d{3})\d{11}$/
+    }
+
+    for(let key in regExpFranchise) {
+
+      return (regExpFranchise[key].test(creditCardNumber) === false )? 'Marca no existe ' : key;
+
+    }
+  }
 };
 
 export default validator;
